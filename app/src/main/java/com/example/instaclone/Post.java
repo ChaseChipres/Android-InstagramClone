08883e808576156b887d80a47422c5f24503925f
Chase Chipres <chasechipres@gmail.com>
2020-02-23 20:09:48 -0800

    Added fragments for posting, profile, and feed.

--- app/src/main/java/com/example/instaclone/Post.java
+++ app/src/main/java/com/example/instaclone/Post.java
@@ -5,13 +5,13 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
-
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
